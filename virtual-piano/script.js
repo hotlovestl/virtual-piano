d7f278ee6d14054d042bef35b6b97b9c6467c36c
hotlovestl <pluhin34@gmail.com>
2021-11-04 02:35:46 +0300

    feat: add active state to piano-keys(click)

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -3,9 +3,20 @@ const pianoKeys = document.querySelector('.piano-key');
 const audio = document.querySelector('.audio');
 
 piano.addEventListener('mousedown', playAudio);
+piano.addEventListener('mousedown', makeKeyActive);
 
 function playAudio(event) {
   audio.src = `assets/audio/${event.target.dataset.note}.mp3`
   audio.currentTime = 0;
   audio.play();
+}
+
+function makeKeyActive(event) {
+  event.target.classList.add('piano-key-active');
+  event.target.addEventListener('mouseup', makeKeyInactive);
+  event.target.addEventListener('mouseout', makeKeyInactive);
+}
+
+function makeKeyInactive(event) {
+  event.target.classList.remove('piano-key-active');
 }
