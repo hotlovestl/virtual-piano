ac3ee8733c3e249b2290820b1d471b856e1f83b7
hotlovestl <pluhin34@gmail.com>
2021-11-04 02:44:34 +0300

    feat: add play by key press

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -4,9 +4,18 @@ const audio = document.querySelector('.audio');
 
 piano.addEventListener('mousedown', playAudio);
 piano.addEventListener('mousedown', makeKeyActive);
+window.addEventListener('keydown', playAudioKeyboard);
 
 function playAudio(event) {
   audio.src = `assets/audio/${event.target.dataset.note}.mp3`
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function playAudioKeyboard(event) {
+  if(event.repeat) return;
+  const pianoKey = document.getElementById(event.code);
+  audio.src = `assets/audio/${pianoKey.dataset.note}.mp3`
   audio.currentTime = 0;
   audio.play();
 }
